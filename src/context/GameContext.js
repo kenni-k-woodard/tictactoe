3c427002e647ec3d4a1a77f1ba9bfe205c8a9bbf
Kenni Woodard <kennikristine@gmail.com>
2023-01-26 11:12:58 -0800

    initialized state for currentplayer, active, and message

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -2,9 +2,27 @@ import { createContext, useContext, useState } from 'react';
 const GameContext = createContext();
 const GameProvider = ({ children }) => {
   // in here we can define any state variables we want to use
-  const [board, setBoard] = useState();
-
-  return <GameContext.Provider value={{ board, setBoard }}>{children}</GameContext.Provider>;
+  const [board, setBoard] = useState([
+    { space: 0, content: '' },
+    { space: 1, content: '' },
+    { space: 2, content: '' },
+    { space: 3, content: '' },
+    { space: 4, content: '' },
+    { space: 5, content: '' },
+    { space: 6, content: '' },
+    { space: 7, content: '' },
+    { space: 8, content: '' },
+  ]);
+  const [current, setCurrent] = useState('X');
+  const [active, setActive] = useState(true);
+  const [message, setMessage] = useState('');
+  return (
+    <GameContext.Provider
+      value={{ board, setBoard, current, setCurrent, active, setActive, message, setMessage }}
+    >
+      {children}
+    </GameContext.Provider>
+  );
 };
 
 const useGameContext = () => {
